57737d4fb293e62ba3455db4bf457f8eb8f775eb
chasrouni <chassrouny@gmail.com>
2023-09-24 14:22:35 +0300

    Added MarvelCharacterTableCell

---
+++ Marvel/Scenes/MarvelCharacters/View/MarvelCharacterTableCell.swift
@@ -0,0 +1,37 @@
+//
+//  MarvelCharacterTableCell.swift
+//  Marvel
+//
+//  Created by Charbel Hasrouni on 24/09/2023.
+//
+
+import UIKit
+import Kingfisher
+
+class MarvelCharacterTableCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var thumbnailImgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        titleLbl.textColor = .black
+        titleLbl.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        thumbnailImgView.contentMode = .scaleAspectFit
+    }
+    
+    func setup(title: String?, thumbnailUrl: String?) {
+        thumbnailImgView.image = nil
+        titleLbl.text = nil
+        
+        thumbnailImgView.kf.indicatorType = .activity
+        if let thumbnailUrl {
+            thumbnailImgView.kf.setImage(with: URL(string: thumbnailUrl),
+                                         options: [.transition(.fade(0.25)),
+                                                   .processor(DownsamplingImageProcessor(size: CGSize(width: 150, height: 150)))]
+            )
+        }
+    }
+}
